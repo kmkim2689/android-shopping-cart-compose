61279652f244a080683c8c51627de809bce010e6
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-10-20 16:57:45 +0900

    feat: 필요한 색상 추가 및 테마 색상 변경

--- app/src/main/java/nextstep/shoppingcart/ui/theme/Color.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/theme/Color.kt
@@ -9,3 +9,14 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Blue80 = Color(0xFFC1C0FF)
+val BlueGrey80 = Color(0xFFE0E0E0)
+
+val Blue50 = Color(0xFF2196F3)
+val BlueGrey50 = Color(0xFF455A64)
+
+val Blue40 = Color(0xFF1E88E5)
+val BlueGrey40 = Color(0xFF37474F)
+
+
