66aea1648e25bcaa1710cc55b7b7e1505b03bcb6
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-10-20 22:25:24 +0900

    refactor: 홈 화면 액티비티명 수정

---
+++ app/src/main/java/nextstep/shoppingcart/HomeActivity.kt
@@ -0,0 +1,19 @@
+package nextstep.shoppingcart
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
+
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ShoppingCartTheme {
+            }
+        }
+    }
+}
+
+
+
